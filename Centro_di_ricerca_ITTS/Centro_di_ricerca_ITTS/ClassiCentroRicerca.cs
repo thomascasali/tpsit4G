bc7333e226b838d7f6dee01e5c7b527a88afd70a
Thomas Casali <casali.thomas@gmail.com>
2020-12-07 09:11:55 +0100

    primo commit Centro di Ricerca

---
+++ Centro_di_ricerca_ITTS/Centro_di_ricerca_ITTS/ClassiCentroRicerca.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Centro_di_ricerca_ITTS
+{
+
+    class CentroDiRicerca
+    {
+        string nome;
+        string citta;
+        public List<AreaDiRicerca> areeRicerca;
+        public CentroDiRicerca(string nome, string citta)
+        {
+            this.nome = nome;
+            areeRicerca = new List<AreaDiRicerca>();
+        }
+
+        public void AddAreaDiRicerca(AreaDiRicerca a)
+        {
+            areeRicerca.Add(a);
+        }
+
+    }
+    class AreaDiRicerca
+    {
+        string nome;
+        string descrizione;
+        double estensione;
+        ResponsabileArea responsabile;
+        public List<Senior> ricecatoriSenior; //elenco di tutti i ricercatori senior dell'area
+        public List<Junior> ricecatoriJunior; //elenco di tutti i ricercatori senior dell'area
+        public List<Progetto> progettiArea; //elenco di tutti i progetti
+        public AreaDiRicerca(string nome, string descrizione)
+        {
+            this.nome = nome;
+            this.descrizione = descrizione;
+            ricecatoriSenior = new List<Senior>();
+            ricecatoriJunior = new List<Junior>();
+            progettiArea = new List<Progetto>();
+        }
+        public string Nome
+        {
+            get { return nome; }
+        }
+        public int NumRicercatoriJunior
+        {
+            get { return ricecatoriJunior.Count(); }
+        }
+        public int NumRicercatoriSenior
+        {
+            get { return ricecatoriSenior.Count(); }
+        }
+        public void AddRicercatoreSenior(Senior s)
+        {
+            ricecatoriSenior.Add(s);
+        }
+        public void AddRicercatoreJunior(Junior j)
+        {
+            ricecatoriJunior.Add(j);
+        }
+        public void AddProgetto(Progetto p)
+        {
+            progettiArea.Add(p);
+        }
+        public void SetResponsabile(ResponsabileArea ra)
+        {
+            responsabile = ra;
+        }
+
+        public Senior GetRicercatoreSeniorDaCodice(int codice)
+        {
+            foreach (Senior s in ricecatoriSenior)
+            {
+                if (s.Codice == codice)
+                {
+                    return s;
+                }
+            }
+            return null;
+        }
+        public Junior GetRicercatoreJuniorDaCodice(int codice)
+        {
+            foreach (Junior j in ricecatoriJunior)
+            {
+                if (j.Codice == codice)
+                {
+                    return j;
+                }
+            }
+            return null;
+        }
+    }
+
+    class Progetto
+    {
+        string nome;
+        string descrizione;
+        public TeamDiRicerca team;
+
+        public Progetto(string n, string d, TeamDiRicerca t)
+        {
+            nome = n;
+            descrizione = d;
+            team = t;
+        }
+
+        public void SetDescrizione(string d)
+        {
+            descrizione = d;
+        }
+        public string Nome
+        {
+            get { return nome; }
+        }
+        public string Descrizione
+        {
+            get { return descrizione; }
+        }
+    }
+
+    class Persona
+    {
+        protected string nome;
+        protected string cognome;
+        protected DateTime dataNascita;
+
+        public Persona(string nome, string cognome, string dataNascita)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.dataNascita = Convert.ToDateTime(dataNascita);
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+        }
+        public string Cognome
+        {
+            get { return cognome; }
+        }
+        public DateTime DataNascita
+        {
+            get { return dataNascita; }
+        }
+        public int GetEta()
+        {
+            return Convert.ToInt32((DateTime.Now.Date - dataNascita).TotalDays / 365);
+        }
+    }
+    class ResponsabileArea : Persona
+    {
+        int maxOreLavoro;
+        public ResponsabileArea(string nome, string cognome, string dataNascita, int maxOreLavoro) : base(nome, cognome, dataNascita)
+        {
+            this.maxOreLavoro = maxOreLavoro;
+        }
+    }
+    class Ricercatore : Persona
+    {
+        protected int codice;
+        protected string specializzazione;
+        protected double stipendio;
+
+        public Ricercatore(int codice, string nome, string cognome, string dataNascita, string spec) : base(nome, cognome, dataNascita)
+        {
+            this.codice = codice;
+            specializzazione = spec;
+        }
+
+        public int Codice
+        {
+            get { return codice; }
+        }
+    }
+
+    class Junior : Ricercatore
+    {
+        private int oreLavoro;
+        public Junior(int codice, string nome, string cognome, string dataNascita, string specializzazione, int oreLavoro) : base(codice, nome, cognome, dataNascita, specializzazione)
+        {
+            this.oreLavoro = oreLavoro;
+        }
+    }
+
+    class Senior : Ricercatore
+    {
+        private int numPubblicazioni;
+        public Senior(int codice, string nome, string cognome, string dataNascita, string specializzazione, int p) : base(codice, nome, cognome, dataNascita, specializzazione)
+        {
+            numPubblicazioni = p;
+        }
+    }
+
+    class TeamDiRicerca
+    {
+        string nomeTeam;
+        public Senior responsabile;
+        public List<Junior> ricercatori;
+
+        public TeamDiRicerca(string nome)
+        {
+            nomeTeam = nome;
+            ricercatori = new List<Junior>();
+        }
+        public string NomeTeam
+        {
+            get { return nomeTeam; }
+            set { nomeTeam = value; }
+        }
+        public void SetResponsabile(Senior resp)
+        {
+            responsabile = resp;
+        }
+        public void AddRicercatore(Junior ric)
+        {
+            ricercatori.Add(ric);
+        }
+        public bool RemoveRicercatore(Junior ric)
+        {
+            foreach (Junior j in ricercatori)
+            {
+                if (j == ric)
+                {
+                    ricercatori.Remove(ric);
+                    return true;
+                }
+            }
+            return false;
+        }
+        public int NumRicercatori()
+        {
+            return ricercatori.Count;
+        }
+        public string GetListaRicercatori()
+        {
+            string s = "";
+            foreach (Junior j in ricercatori)
+            {
+                s = s + j.Nome + " " + j.Cognome + ";";
+            }
+            return s;
+        }
+
+    }
+
+}
---
+++ Centro_di_ricerca_ITTS/Centro_di_ricerca_ITTS/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Centro_di_ricerca_ITTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creazione del centro di ricerca
+            var cr = new CentroDiRicerca("Centro di ricerca ITTS", "Rimini");
+            //creazione delle aree di ricerca
+            cr.AddAreaDiRicerca(new AreaDiRicerca("Informatica", "Sviluppo linguaggi di programmazione"));
+            cr.AddAreaDiRicerca(new AreaDiRicerca("Telecomunicazioni", "Sviluppo dei sistemi di telecomunicazione"));
+            cr.AddAreaDiRicerca(new AreaDiRicerca("Innovazione", "Computer quantici"));
+            //impostazione dei responsabili di area
+            cr.areeRicerca[0].SetResponsabile(new ResponsabileArea("Bill", "Gates", "10/10/1950", 24));
+            cr.areeRicerca[1].SetResponsabile(new ResponsabileArea("Guglielmo", "Marconi", "9/9/1936", 24));
+            cr.areeRicerca[2].SetResponsabile(new ResponsabileArea("Elon", "Musk", "1/3/1980", 24));
+            //assunzione dei ricercatori per aree di competenza
+            cr.areeRicerca[0].AddRicercatoreSenior(new Senior(1, "Thomas", "Casali", "20/03/1972", "Python", 120));
+            cr.areeRicerca[0].AddRicercatoreSenior(new Senior(2, "Fabio", "Corbelli", "10/04/1957", "C#", 153));
+            cr.areeRicerca[0].AddRicercatoreSenior(new Senior(3, "Fabio", "Ugolini", "14/06/1967", "VB.Net", 133));
+            cr.areeRicerca[0].AddRicercatoreJunior(new Junior(4, "Mario", "Rossi", "22/03/1992", "linguaggi", 20));
+            cr.areeRicerca[0].AddRicercatoreJunior(new Junior(5, "Riccardo", "Verdi", "21/03/1992", "linguaggi", 20));
+            cr.areeRicerca[0].AddRicercatoreJunior(new Junior(6, "Alan", "Bianchi", "12/03/1992", "linguaggi", 20));
+            cr.areeRicerca[0].AddRicercatoreJunior(new Junior(7, "Demis", "Raschia", "13/03/1992", "linguaggi", 20));
+
+            //stampo il numero di ricercatori presenti in ogni area
+            foreach (AreaDiRicerca a in cr.areeRicerca)
+            {
+                Console.WriteLine($"Nell'area di ricerca {a.Nome.ToUpper()} ci sono {a.NumRicercatoriJunior} ricercatori Junior e {a.NumRicercatoriSenior} ricercatori Senior");
+            }
+
+            //mi creo una variabile di riferimento all'area di ricerca di informatica
+            var aInfo = cr.areeRicerca[0];
+
+            //creo un team di ricerca da assegnare ad un progetto
+            var team = new TeamDiRicerca("Team di ricerca Info1");
+            team.SetResponsabile(aInfo.GetRicercatoreSeniorDaCodice(1));
+            team.AddRicercatore(aInfo.GetRicercatoreJuniorDaCodice(4));
+            team.AddRicercatore(aInfo.GetRicercatoreJuniorDaCodice(5));
+
+            //creazione di un progetto
+            aInfo.AddProgetto(new Progetto("Python 4", "Versione 4.0", team));
+
+            //stampa tutti i progetti in corso con il team che segue il progetto
+            foreach (AreaDiRicerca a in cr.areeRicerca)
+            {
+                Console.WriteLine($"\nNell'area di ricerca {a.Nome.ToUpper()} sono attivi i seguenti progetti:");
+                int numProgetto = 1;
+                foreach (Progetto p in a.progettiArea)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($" {numProgetto}) {p.Nome}");
+                    Console.ResetColor();
+                    Console.WriteLine($"  il team {p.team.NomeTeam.ToUpper()} che segue il progetto è composto da:");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write($"  {p.team.responsabile.Nome} {p.team.responsabile.Cognome}");
+                    Console.ResetColor();
+                    Console.WriteLine(" - Responsabile progetto");
+                    int numRicercatori = 1;
+                    foreach (Junior j in p.team.ricercatori)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        Console.Write($"  {j.Nome} {j.Cognome}");
+                        Console.ResetColor();
+                        Console.WriteLine($" - Ricercatore {numRicercatori}");
+                        numRicercatori++;
+                    }
+                    numProgetto++;
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
